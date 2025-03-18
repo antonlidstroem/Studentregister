29a340667150f13959d5aae9364076ea6d5b5c0d
Anton Lidström <antonlidstroem@outlook.com>
2025-03-18 11:25:58 +0100

    Lagt till loggning i output-konsolen. Även lagt till två andra loggnings-funktioner som är bortkommenterade.

--- Studentregister/StudentDbContext.cs
+++ Studentregister/StudentDbContext.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Debug;
 using static System.Net.Mime.MediaTypeNames;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
@@ -12,12 +15,29 @@ namespace Studentregister
 {
     internal class StudentDbContext : DbContext
     {
-        //private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog = StudentRegister; Integrated Security = True; Connect Timeout = 30; Encrypt=False;Trust Server Certificate=False;Application Intent = ReadWrite; Multi Subnet Failover=False"; 
+        /* Hårdkodad connectionString
+         private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog = StudentRegister; Integrated Security = True; Connect Timeout = 30; Encrypt=False;Trust Server Certificate=False;Application Intent = ReadWrite; Multi Subnet Failover=False"; 
+        */
+
         public DbSet<Student> Students { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(new ConfigurationBuilder().AddJsonFile("appsettings.json")
                 .Build().GetSection("ConnectionStrings")["StudentDb"]);
+
+            /* LOGGNING ALTERNATIV 1
+            optionsBuilder.UseLoggerFactory(new LoggerFactory(
+                new[] {
+                   new DebugLoggerProvider()
+            }));
+            */
+
+            /* LOGGNING ALTERNATIV 2
+            optionsBuilder.LogTo(Console.WriteLine);
+            */
+
+            //LOGGNING ALTERNATIV 3
+            optionsBuilder.LogTo(message => Debug.WriteLine(message));
         }
     }
 }
